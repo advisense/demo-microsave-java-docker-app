bf654c9be9e56f1ec15618980352d80e98bbb150
Arne Berner <aberner@practical.no>
2025-01-06 19:53:11 +0100

    Update IndexController.java

--- src/main/java/com/advisense/microsave/controller/IndexController.java
+++ src/main/java/com/advisense/microsave/controller/IndexController.java
@@ -6,19 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.Random;
 
 @RestController
 public class IndexController {
     private Logger logger = LoggerFactory.getLogger(IndexController.class);
+    private Random random = new Random();
 
     @GetMapping("/")
     public HashMap index() {
-        logger.debug("request served");
+        int id = random.nextInt(10000);
+        logger.debug("request served: "+ id);
 
         return new HashMap<String,Object>() {{
             put("success", true);
             put("hello", "world");
             put("new-message", "adding a new msg");
+            put("message-id", id);
         }};
     }
 }
